d692e35b0934e4fa61865f5025d5ab23fd87eb98
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2023-09-16 19:25:41 -0300

    modifiquei as views para usarem a views genericas do rest_framework

--- testemunho/urls.py
+++ testemunho/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ComentarioAPIView, TestemunhoAPIView
+from .views import ComentarioAPIView, ComentariosAPIView, TestemunhoAPIView, TestemunhosAPIView
 
 urlpatterns = [
-    path('testemunhos/', TestemunhoAPIView.as_view(), name='testemunhos'),
-    path('comentarios/', ComentarioAPIView.as_view(), name='comentarios'),
+    path('testemunhos/', TestemunhosAPIView.as_view(), name='testemunhos'),
+    path('comentarios/', ComentariosAPIView.as_view(), name='comentarios'),
+    path('testemunhos/<int:pk>/', TestemunhoAPIView.as_view(), name='testemunho'),
+    path('comentarios/<int:pk>/', ComentarioAPIView.as_view(), name="comentario")
 ]
--- testemunho/views.py
+++ testemunho/views.py
@@ -3,10 +3,20 @@ from rest_framework import generics
 from .models import Testemunho, Comentario
 from .serializers import TestemunhoSerializer, ComentarioSerializer
 
-class TestemunhoAPIView(generics.ListCreateAPIView):
+# ListCreateAPIView: tanto lista quanto criar não depende de nenhum id
+class TestemunhosAPIView(generics.ListCreateAPIView):
     queryset = Testemunho.objects.all()
     serializer_class = TestemunhoSerializer
 
-class ComentarioAPIView(generics.ListCreateAPIView):
+# RetrieveUpdateDestroyAPIView: tanto pegar um ..., quanto atualizar ou deletar depende de um id 
+class TestemunhoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Testemunho.objects.all()
+    serializer_class = TestemunhoSerializer
+
+class ComentariosAPIView(generics.ListCreateAPIView):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
+
+class ComentarioAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
