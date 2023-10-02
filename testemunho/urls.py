1524af997021f24826ab59f66ae0305c50978c64
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2023-10-02 15:10:45 -0300

    utilizei os metodos genericos do rest_frawork

--- testemunho/urls.py
+++ testemunho/urls.py
@@ -4,7 +4,11 @@ from .views import ComentarioAPIView, ComentariosAPIView, TestemunhoAPIView, Tes
 
 urlpatterns = [
     path('testemunhos/', TestemunhosAPIView.as_view(), name='testemunhos'),
-    path('comentarios/', ComentariosAPIView.as_view(), name='comentarios'),
     path('testemunhos/<int:pk>/', TestemunhoAPIView.as_view(), name='testemunho'),
-    path('comentarios/<int:pk>/', ComentarioAPIView.as_view(), name="comentario")
+
+    path('testemunhos/<int:testemunho_pk>/comentarios/', ComentariosAPIView.as_view(), name='testemunho_comentarios'),
+    path('testemunhos/<int:testemunho_pk>/comentarios/<int:comentario_pk>/', ComentarioAPIView.as_view(), name='testemunho_comentario'),
+
+    path('comentarios/', ComentariosAPIView.as_view(), name='comentarios'),
+    path('comentarios/<int:comentario_pk>/', ComentarioAPIView.as_view(), name="comentario")
 ]
--- testemunho/views.py
+++ testemunho/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Testemunho, Comentario
 from .serializers import TestemunhoSerializer, ComentarioSerializer
@@ -17,6 +18,19 @@ class ComentariosAPIView(generics.ListCreateAPIView):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('testemunho_pk'):
+            return self.queryset.filter(testemunho_id=self.kwargs.get('testemunho_pk'))
+        return self.queryset.all()
+
 class ComentarioAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
+
+    def get_object(self):
+        if self.kwargs.get('testemunho_pk'):
+            return get_object_or_404(self.get_queryset(), 
+                                     testemunho_id=self.kwargs.get('testemunho_pk'),
+                                     pk=self.kwargs.get('comentario_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('comentario_pk'))
+    
