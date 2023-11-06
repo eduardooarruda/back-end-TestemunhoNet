e34f4a921489e11c0fcc5897a2095d2616ea4eaa
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2023-11-06 19:12:07 -0300

    criei os modelos das entidades

--- testemunho/models.py
+++ testemunho/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Usuario(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
 class Testemunho(models.Model):
     titulo = models.CharField(max_length=300)
     conteudo = models.CharField(max_length=3000)
-    dataCriacao = models.DateTimeField(auto_now=True)
-    autor = models.CharField(max_length=100)
-    comentariosAtivo = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now=True)
+    autor = models.ForeignKey(Usuario, related_name="testemunhos", on_delete=models.CASCADE)
+    comentarios_ativo = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = 'testemunho'
@@ -19,16 +27,28 @@ class Testemunho(models.Model):
     
 
 class Comentario(models.Model):
+    data_criacao = models.DateTimeField(auto_now=True)
+    conteudo = models.CharField(max_length=1000)
     testemunho = models.ForeignKey(Testemunho, related_name="comentarios", on_delete=models.CASCADE)
-    autor = models.CharField(max_length=100)
-    conteudo = models.CharField(max_length=600)
+    comentario_pai = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Usuario, related_name="comentarios", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'comentário'
         verbose_name_plural = 'comentários'
         ordering = ['id'] #ordernação crescente
         #ordering = ['-id'] #ordernação decrescente
+        
 
     def __str__(self) -> str:
         return f'{self.autor} comentou no testemunho {self.testemunho}'
 
+class Like(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    testemunho = models.ForeignKey(Testemunho, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('usuario', 'testemunho')
+
+    def __str__(self):
+        return f'{self.usuario} curtiu o testemunho {self.testemunho}'
