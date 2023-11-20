f9329c91a236cd109094c766b1a65ec78eaa5a37
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2023-11-20 16:04:29 -0300

    modifiquei as classes TestemunhoAdmin para mostrar um campo de busca pelo título, para filtrar pelos comentarios ativos e adicionei outros campos de quantidade de comentarios e likes, além disso na classe UsuarioAdmin eu coloquei um campo de busca pelo nome

--- testemunho/admin.py
+++ testemunho/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from .models import Usuario, Testemunho, Comentario, Like
 
+
+
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email')
+    search_fields = ('nome',)
 
 @admin.register(Testemunho)
 class TestemunhoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'conteudo', 'data_criacao', 'autor', 'comentarios_ativo')
+    list_display = ('titulo', 'conteudo', 'data_criacao', 'autor', 'comentarios_ativo', 'quantidade_comentarios', 'quantidade_likes')
+    search_fields = ('titulo',)
+    list_filter = ('comentarios_ativo',)
+
+    @admin.display(description="Quantidade de comentários")
+    def quantidade_comentarios(self, obj):
+        return obj.comentarios.count()
+    
+    @admin.display(description="Quantidade de likes")
+    def quantidade_likes(self, obj):
+        return obj.likes.count()
+
 
 @admin.register(Comentario)
 class ComentarioAdmin(admin.ModelAdmin):
